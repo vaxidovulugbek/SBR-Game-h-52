cef44af88c7fabf2154d57b71a8c649baa8e31b0
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-02-18 09:01:52 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let modal = document.getElementById("modal");
+let openBtn = document.getElementById("modbtn");
+
+
+openBtn.addEventListener("click", () =>{
+  console.log("ok");
+  modal.classList.toggle("show");
+})
+
+
